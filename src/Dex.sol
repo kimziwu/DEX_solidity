e8e97feb31242176bc8f0efad20fd22fc2bdcf10
kimziwu <jiwoo.kim@dream-academy.io>
2023-03-20 14:43:24 +0900

    first commit

--- src/Dex.sol
+++ src/Dex.sol
@@ -27,56 +27,11 @@ contract Dex is ERC20 {
     }
 
     function addLiquidity(uint256 tokenXAmount, uint256 tokenYAmount, uint256 minimumLPTokenAmount) external returns (uint256 LPTokenAmount){
-        require(tokenXAmount>0 && tokenYAmount>0, "amount > 0");
-        require(_tokenX.allowance(msg.sender, address(this))>=tokenXAmount,"ERC20: insufficient allowance");
-        require(_tokenY.allowance(msg.sender, address(this))>=tokenYAmount,"ERC20: insufficient allowance");
-        require(_tokenX.balanceOf(msg.sender)>=tokenXAmount,"ERC20: transfer amount exceeds balance");
-        require(_tokenY.balanceOf(msg.sender)>=tokenYAmount,"ERC20: transfer amount exceeds balance");
-
-        uint lpToken; 
-        if (liquiditySum==0){
-            lpToken=tokenXAmount*tokenYAmount/_demical;
-        }
-        else {
-            uint X=_tokenX.balanceOf(address(this));
-            uint Y=_tokenY.balanceOf(address(this));
-
-            uint liquidityX=liquiditySum*tokenXAmount/X;
-            uint liquidityY=liquiditySum*tokenYAmount/Y;
-            lpToken=(liquidityX<liquidityY)?liquidityX:liquidityY;
-        }
-
-        require(lpToken>=minimumLPTokenAmount,"minimum lpToken");
-        liquiditySum+=lpToken;
-        liquidityUser[msg.sender]+=lpToken;
-
-        _tokenX.transferFrom(msg.sender, address(this), tokenXAmount);
-        _tokenY.transferFrom(msg.sender, address(this), tokenYAmount);
-        transfer(msg.sender, lpToken);
         
-        return lpToken;
     }
 
     function removeLiquidity(uint256 LPTokenAmount, uint256 minimumTokenXAmount, uint256 minimumTokenYAmount) external returns (uint, uint) {
-        require(LPTokenAmount>0,"amount>0");
-        require(liquidityUser[msg.sender]>=LPTokenAmount,"more than liquidity");
-
-        uint tokenXAmount=_tokenX.balanceOf(address(this));
-        uint tokenYAmount=_tokenY.balanceOf(address(this));
-        uint X=tokenXAmount*LPTokenAmount/liquiditySum;
-        uint Y=tokenYAmount*LPTokenAmount/liquiditySum;
-
-        require(X>=minimumTokenXAmount,"less than minimum tokenX");
-        require(Y>=minimumTokenYAmount,"less than mininum tokenY");
-
-        liquiditySum-=LPTokenAmount;
-        liquidityUser[msg.sender]-=LPTokenAmount;
-
-        _tokenX.transfer(msg.sender, X);
-        _tokenY.transfer(msg.sender, Y);
-        _burn(msg.sender, LPTokenAmount);
-
-        return (X,Y);
+        
     }
 
   
