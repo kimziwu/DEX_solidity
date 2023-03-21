faf2a620e23ae46b52bdfea56f65765df349a709
kimziwu <jiwoo.kim@dream-academy.io>
2023-03-22 04:05:35 +0900

    test

--- src/Dex.sol
+++ src/Dex.sol
@@ -1,23 +1,49 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+
+/**
+CPMM (xy=k) 방식의 AMM을 사용하는 DEX를 구현
+
+- Swap
+Pool 생성 시 지정된 두 종류의 토큰을 서로 교환 가능
+수량이 0인 토큰으로 스왑
+Input 토큰과 Input 수량, 최소 Output 요구량을 받아서 Output 토큰으로 바꿔주고,
+최소 요구량에 미달할 경우 revert 해야함
+수수료는 0.1%
+
+ex: X->Y swap
+스왑되는 토큰 X 양을 계산 : tokenXAmout*999/1000
+    99.9%(0.1% 수수료)
+
+- Add / Remove Liquidity
+ERC-20 기반 LP 토큰을 사용
+수수료 수입과 Pool에 기부된 금액을 제외하고는 더 많은 토큰을 회수할 수 있는 취약점이 없어야 함
+Concentrated Liquidity는 필요 없습니다.
+
+- function swap(uint256 tokenXAmount, uint256 tokenYAmount, uint256 tokenMinimumOutputAmount) external returns (uint256 outputAmount);
+tokenXAmount / tokenYAmount 중 하나는 무조건 0이어야 합니다. 수량이 0인 토큰으로 스왑됨.
+
+- function addLiquidity(uint256 tokenXAmount, uint256 tokenYAmount, uint256 minimumLPTokenAmount) external returns (uint256 LPTokenAmount);
+- function removeLiquidity(uint256 LPTokenAmount, uint256 minimumTokenXAmount, uint256 minimumTokenYAmount) external;
+- function transfer(address to, uint256 lpAmount) external returns (bool);
+*/
+
+
 import "forge-std/console.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract Dex is ERC20 {
-    ERC20 public _tokenX;
-    ERC20 public _tokenY;
-    ERC20 public _tokenLP;
-    uint public _demical;
-    uint public liquiditySum; //totalSupply()?
+    ERC20 _tokenX;
+    ERC20 _tokenY;
+    uint _demical;
+    uint liquiditySum; //totalSupply()
     
-    mapping(address=>uint) public liquidityUser; 
+    mapping(address=>uint) liquidityUser; 
 
     constructor(address tokenX, address tokenY) ERC20("LPToken","LPT"){
         _tokenX=ERC20(tokenX);
         _tokenY=ERC20(tokenY);
-        _tokenLP=ERC20(address(this));
-
         _demical=10**18;
     }
 
@@ -27,8 +53,7 @@ contract Dex is ERC20 {
         uint X=_tokenX.balanceOf(address(this));
         uint Y=_tokenY.balanceOf(address(this));
 
-        console.log("X,Y",X, Y);
-        require(X>0 && Y>0 , "no swap amount");
+        require(X>0 && Y>0);
 
         uint input_amount;
         uint output_amount;
@@ -36,23 +61,22 @@ contract Dex is ERC20 {
         ERC20 output;
 
 
-        // swap 
-        // fee : 0.1%
-        if (tokenXAmount>0){ // want X token
-            input=_tokenX;
+        // swap, fee : 0.1%
+        if (tokenXAmount>0){ // swap Y token
+            input=_tokenX; 
             output=_tokenY;
             input_amount=tokenXAmount;
             output_amount=Y*(tokenXAmount*999/1000)/(X+(tokenXAmount*999/1000));
-            //console.log("amount:",amount);
+            
         }
-        else { // want Y token
+        else { // swap X token
             input=_tokenY;
             output=_tokenX;
             input_amount=tokenYAmount;
             output_amount=X*(tokenYAmount*999/1000)/(Y+(tokenYAmount*999/1000));
         }
 
-        require(output_amount>=tokenMinimumOutputAmount,"");
+        require(output_amount>=tokenMinimumOutputAmount);
         
         input.transferFrom(msg.sender, address(this), input_amount);
         output.transfer(msg.sender, output_amount);        
@@ -60,8 +84,9 @@ contract Dex is ERC20 {
         return output_amount;
     }
 
+
     function addLiquidity(uint256 tokenXAmount, uint256 tokenYAmount, uint256 minimumLPTokenAmount) external returns (uint256 LPTokenAmount){
-        require(tokenXAmount>0 && tokenYAmount>0, "amount > 0");
+        require(tokenXAmount>0 && tokenYAmount>0);
         require(_tokenX.allowance(msg.sender, address(this))>=tokenXAmount,"ERC20: insufficient allowance");
         require(_tokenY.allowance(msg.sender, address(this))>=tokenYAmount,"ERC20: insufficient allowance");
         require(_tokenX.balanceOf(msg.sender)>=tokenXAmount,"ERC20: transfer amount exceeds balance");
@@ -80,7 +105,8 @@ contract Dex is ERC20 {
             lpToken=(liquidityX<liquidityY)?liquidityX:liquidityY;
         }
 
-        require(lpToken>=minimumLPTokenAmount,"minimum lpToken");
+        require(lpToken>=minimumLPTokenAmount);
+
         liquiditySum+=lpToken;
         liquidityUser[msg.sender]+=lpToken;
 
@@ -91,9 +117,10 @@ contract Dex is ERC20 {
         return lpToken;
     }
 
+
     function removeLiquidity(uint256 LPTokenAmount, uint256 minimumTokenXAmount, uint256 minimumTokenYAmount) external returns (uint, uint) {
-        require(LPTokenAmount>0,"amount>0");
-        require(liquidityUser[msg.sender]>=LPTokenAmount,"more than liquidity");
+        require(LPTokenAmount>0);
+        require(liquidityUser[msg.sender]>=LPTokenAmount);
 
         uint tokenXAmount=_tokenX.balanceOf(address(this));
         uint tokenYAmount=_tokenY.balanceOf(address(this));
@@ -118,6 +145,4 @@ contract Dex is ERC20 {
         _mint(to, lpAmount);
         return true;
     }
-
-   // math.?
 }
